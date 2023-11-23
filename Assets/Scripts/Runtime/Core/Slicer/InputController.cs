cbaebc23cf6ded9c53c5eef8be46fa8c1a6de358
Sklyarov Olexandr <alexskrat@gmail.com>
2023-11-23 02:18:30 +0200

    [Update] Remove input actions.

--- Assets/Scripts/Runtime/Core/Slicer/InputController.cs
+++ Assets/Scripts/Runtime/Core/Slicer/InputController.cs
@@ -5,22 +5,6 @@ namespace SA.Runtime.Core.Slicer
 {
     public class InputController : MonoBehaviour
     {
-        // private PlayerInputAction _inputAction;
-        // private bool _isPressed;
-
-        //public bool IsPressed => Touchscreen.current.primaryTouch.press.isPressed;
-        public bool IsPressed => Touchscreen.current.primaryTouch.press.isPressed;
-        // {
-        //     _inputAction = new PlayerInputAction();  
-        //     _inputAction.Enable();   
-
-        //     _inputAction.Touch.PointerPress.started += (_) => {_isPressed = true;}; 
-        //     _inputAction.Touch.PointerPress.canceled += (_) => {_isPressed = false;};  
-        // }
-
-        // private void OnDestroy() 
-        // {
-        //     _inputAction?.Disable(); 
-        // }
+        public bool IsPressed => Touchscreen.current.primaryTouch.press.isPressed;        
     }
 }
